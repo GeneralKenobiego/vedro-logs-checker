6bc76c5e7098cc3f39e5a913bfea2c2a4b27c9ae
GeneralKenobiego <disgreensquid@gmail.com>
2025-12-04 16:42:25 +0700

    package init fix

--- vedro_logs_checker/__init__.py
+++ vedro_logs_checker/__init__.py
@@ -1,4 +1,4 @@
-from .vedro_logs_checker import VedroLogsChecker
+from .vedro_logs_checker import VedroLogsChecker, skip_logs_check
 
-__version__ = "0.0.8"
-__all__ = ["VedroLogsChecker"]
+__version__ = "0.0.9"
+__all__ = ["VedroLogsChecker", "skip_logs_check"]
